051e7d675ed98c8fd4386652750f38055615a3a2
nuxion <nuxion@gmail.com>
2022-11-30 11:57:13 -0300

    youtube: testing for search and video parsing

--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -1,8 +1,22 @@
 from datahtml import youtube
 
 
-def test_transform():
-    with open("tests/test.html", "r") as f:
+def test_transform_channel():
+    with open("tests/youtube_channel.html", "r") as f:
         data = f.read()
     parsed = youtube.transform_channel(data)
     assert isinstance(parsed, youtube.ChannelMeta)
+
+
+def test_transform_video():
+    with open("tests/youtube_video.html", "r") as f:
+        data = f.read()
+    parsed = youtube.transform_video(data)
+    assert isinstance(parsed, youtube.Video)
+
+
+def test_transform_search():
+    with open("tests/youtube_search.html", "r") as f:
+        data = f.read()
+    parsed = youtube.transform_search(data)
+    assert isinstance(parsed[0], youtube.SearchVideo)
