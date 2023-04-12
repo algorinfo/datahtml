052113f6b8ced6586d8a7a6d7b8ca33a107376d3
nuxion <nuxion@gmail.com>
2023-04-12 09:54:57 -0300

    fix tests

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,11 +1,11 @@
-from datahtml import parsers, root
+from datahtml import parsers, sitemap
 
 
 def test_root_sitemap_sitemaps():
     with open("tests/root_carrefour_sitemap.xml", "r") as f:
         data = f.read()
     soup = parsers.text2soup(data)
-    sites = root.sitemap_sitemaps(soup, filter_dt=None)
+    sites = sitemap.sitemap_sitemaps(soup, filter_dt=None)
 
     assert len(sites) > 0
 
@@ -14,6 +14,6 @@ def test_root_sitemap_from_robots():
     with open("tests/robots.txt", "r") as f:
         data = f.read()
 
-    sites = root.get_sitemaps_from_robots(data)
+    sites = sitemap.get_sitemaps_from_robots(data)
 
     assert len(sites) > 0
